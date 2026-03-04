139980b91ef89d405daca483cdb7db97216c03a3
kanhaiyamishra05 <kanhaiyakumar993197@gmail.com>
2026-03-04 18:58:48 +0530

    Add files via upload

---
+++ checknumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class checknumber {
+    public static void main(String[] args) {
+        System.out.println("enter number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+    }
+    
+}
